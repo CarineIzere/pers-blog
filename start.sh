578fbcb16a948f9a10ca99668577eec8542a817c
CarineIzere <carizeree@gmail.com>
2019-03-01 12:12:20 +0200

    add class well in index.html

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-export MAIL_USERNAME='carizeree@gmail.com'
-export MAIL_PASSWORD='limousine'
+# export MAIL_USERNAME='carizeree@gmail.com'
+# export MAIL_PASSWORD='limousine'
 export SECRET_KEY='12345'
 export SQLALCHEMY_DATABASE_URL= 'postgresql+psycopg2://wecode:caca1@localhost/pitches'
 python3.6 manage.py server
